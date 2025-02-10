24616f8d6d41e933bdb13f71a2a092a5f3b459b1
gato4x4 <victoramirezserra@gmail.com>
2025-02-10 17:12:03 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="CSSindex.css">
+</head>
+<body>
+    <div class="d-flex justify-content-center align-items-center vh-100 background-storypath">
+        <div>
+            <h1><span class="color-story">Story</span><span class="color-path">Path</span></h1>
+            <p>Crea, comparte y juega historias interactivas donde tus decisiones cambian el final!!!</p>
+            <a href="login.php" class="btn btn-primary">Iniciar Sesion</a>
+            <a href="register.php" class="btn btn-primary">Registrarse</a>
+        </div>
+
+        <div>
+            <img src="logo.png" alt="StoryPath" class="img-fluid">
+        </div>
+        
+    </div>
+    
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
